cbf0ddfff6e8449f8fa63a7a9ae55408c1ca8bcf
guixuejiang <jgx518@163.com>
2024-11-04 19:07:24 +0800

    submit sort() func

--- studentsystem.py
+++ studentsystem.py
@@ -175,7 +175,37 @@ def modify():
     if mark == "y":
         modify()
 def sort():
-    pass
+    show()
+    if os.path.exists(filename):
+        with open(filename, "r") as file:
+            student_old = file.readlines()
+            student_new = []
+            for list in student_old:
+                d = dict(eval(list))
+                student_new.append(d)
+    else:
+        return
+    ascORdesc = input(f"请选择 (0升序; 1降序) : ")
+    if ascORdesc == "0":  # 按照升序排列
+        ascORdescBool = False
+    elif ascORdesc == "1":
+        ascORdescBool = True
+    else:
+        print(f"您输入有误, 请重新输入")
+        sort()
+    mode = input(f"请选择排序方式 (1 按英语成绩排序; 2按Python成绩排序; 3 按C语言成绩排序; 0 按总成绩排序); ")
+    if mode == "1":
+        student_new.sort(key=lambda x:x["english"],reverse=ascORdescBool)
+    elif mode == "2":
+        student_new.sort(key=lambda x:x["python"], reverse=ascORdescBool)
+    elif mode == "3":
+        student_new.sort(key=lambda x:x["c"],reverse=ascORdescBool)
+    elif mode == "0":
+        student_new.sort(key=lambda x:x["english"] + x["python"] + x["c"],reverse=ascORdescBool)
+    else:
+        print(f"您的输入有误,请重新输入!")
+        sort()
+    show_student(student_new)
 
 
 def total():
